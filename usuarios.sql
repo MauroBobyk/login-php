4e7a39ae9d933f0dbbdb00cd7f9f16a2f009ca12
Prof Mauro Bobyk <profe.maurobobyk@gmail.com>
2026-09-18 11:00:34 -0300

    Add copyright and license information to usuarios.sql
    Added copyright notice and license information to usuarios.sql.

--- usuarios.sql
+++ usuarios.sql
@@ -1,6 +1,19 @@
--- La base de datos "login" ya existe en XAMPP.
--- Este script solo crea la tabla de usuarios dentro de ella.
+/*
+  Copyright (C) 2026, Mauro Bobyk.
 
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://gnu.org>.
+*/
 USE login;
 
 CREATE TABLE IF NOT EXISTS usuarios (
